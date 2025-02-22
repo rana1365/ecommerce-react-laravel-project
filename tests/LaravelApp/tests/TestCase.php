32d63e89ad36ec069101a95fcb48360715dc4e35
Julius Kiekbusch <contact@julius-kiekbusch.de>
2025-02-22 09:53:30 +0100

    wip

--- tests/LaravelApp/tests/TestCase.php
+++ tests/LaravelApp/tests/TestCase.php
@@ -6,17 +6,4 @@ use Illuminate\Contracts\Console\Kernel;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
-{
-    /**
-     * Create a new application instance.
-     */
-    public function createApplication(): Application
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-        $app->make(Kernel::class)->bootstrap();
-
-        return $app;
-    }
-}
+abstract class TestCase extends BaseTestCase {}
