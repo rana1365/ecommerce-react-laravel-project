0f73cb04afd828cad4cb32935517f967279f1cb3
rana1365 <rana135105@gmail.com>
2025-04-13 23:26:53 +0200

    Product module implementation done

---
+++ app/Http/Controllers/admin/ProductController.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    // This method will return all the products
+    public function index ()
+    {
+        $product = Product::orderBy('created_at', 'DESC')->get();
+        return response()->json([
+            'status' => 200,
+            'data' => $product
+        ], 200);
+    }
+
+    // This method will create a product into the database
+    public function store (Request $request)
+    {
+        // validate the request
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'category' => 'required|integer',
+            'sku' => 'required|unique:products,sku',
+            'is_featured' => 'required',
+            'status' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        // Store the product into db
+        $product = new Product();
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->compare_price = $request->compare_price;
+        $product->category_id = $request->category;
+        $product->brand_id = $request->brand;
+        $product->description = $request->description;
+        $product->short_description = $request->short_description;
+        $product->sku = $request->sku;
+        $product->qty = $request->qty;
+        $product->barcode = $request->barcode;
+        $product->status = $request->status;
+        $product->is_featured = $request->is_featured;
+        $product->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product has been created successfully'
+        ], 200);
+    }
+
+    // This method will return a single product from db
+    public function show ($id)
+    {
+        $product = Product::find($id);
+
+        if ($product == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found'
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => 200,
+            'data' => $product
+        ], 200);
+
+    }
+
+
+    // This method will update a product
+    public function update ($id, Request $request)
+    {
+        $product = Product::find($id);
+
+        if ($product == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found.'
+            ], 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'price' => 'required|numeric',
+            'category' => 'required|integer',
+            'sku' => 'required|unique:products,sku,'.$id.',id',
+            'is_featured' => 'required',
+            'status' => 'required'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 400,
+                'errors' => $validator->errors()
+            ], 400);
+        }
+
+        // Update the product
+
+        $product->title = $request->title;
+        $product->price = $request->price;
+        $product->compare_price = $request->compare_price;
+        $product->category_id = $request->category;
+        $product->brand_id = $request->brand;
+        $product->description = $request->description;
+        $product->short_description = $request->short_description;
+        $product->sku = $request->sku;
+        $product->qty = $request->qty;
+        $product->barcode = $request->barcode;
+        $product->status = $request->status;
+        $product->is_featured = $request->is_featured;
+        $product->save();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product has been updated successfully.'
+        ], 200);
+
+    }
+
+
+    // This method will delete a product
+    public function destroy ($id)
+    {
+        $product = Product::find($id);
+
+        if ($product == null) {
+            return response()->json([
+                'status' => 404,
+                'message' => 'Product not found',
+                'data' => []
+            ], 404);
+        }
+
+        $product->delete();
+
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product has been deleted successfully.'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\admin\AuthController;
 use App\Http\Controllers\admin\BrandController;
 use App\Http\Controllers\admin\CategoryController;
+use App\Http\Controllers\admin\ProductController;
+use App\Http\Controllers\admin\SizeController;
+use App\Http\Controllers\TempImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,7 @@ Route::post('/admin/login', [AuthController::class, 'authenticate']);
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+
 Route::group(['middleware' => 'auth:sanctum'], function () {
     
     Route::get('categories', [CategoryController::class, 'index']);
@@ -22,5 +26,11 @@ Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::post('categories', [CategoryController::class, 'store']);
 
     Route::resource('brands', BrandController::class);
+
+    Route::resource('products', ProductController::class);
+
+    Route::get('sizes', [SizeController::class, 'index']);
+
+    Route::post('temp-images', [TempImageController::class, 'store']);
 
 });
